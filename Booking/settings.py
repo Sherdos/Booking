08a0c1db721392a1aa45fbf167e3aabd7559c65a
Sherdos <Sherdos26@gmail.com>
2022-10-04 00:25:08 +0600

    Deploy to Heroku

--- Booking/settings.py
+++ Booking/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('django-insecure-*1l!a(tg+h@+ug=kern=fkuqd6s-5m_#ll@6(0_!=yujx&ket+')
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-*1l!a(tg+h@+ug=kern=fkuqd6s-5m_#ll@6(0_!=yujx&ket+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
