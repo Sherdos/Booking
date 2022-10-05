cee60e6400945b4d35ac7b6b506b5e0a5bae12c3
Sherdos <Sherdos26@gmail.com>
2022-10-05 23:13:30 +0600

    tree boto

--- Booking/settings.py
+++ Booking/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 EMAIL_USE_TLS = True 
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587 
-EMAIL_HOST_USER = S3Connection(os.environ['EMAIL_HOST_USER'])
-EMAIL_HOST_PASSWORD = S3Connection(os.environ['EMAIL_HOST_PASSWORD'])
+EMAIL_HOST_USER = 'zaparovsherdos7@gmail.com' 
+EMAIL_HOST_PASSWORD = 'ojczhrbpmmepwhdh'
 
 
 # Static files (CSS, JavaScript, Images)
